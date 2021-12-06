4f55ab9cd4d86f54086a856dcb217d8582006ec7
LeviEyal <happyeyal@gmail.com>
2021-12-06 20:04:58 +0200

    upload all files

---
+++ popup.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const moodleBTN = document.getElementById('moodleBTN');
+  const meidaBTN = document.getElementById('meidaBTN');
+  const pniotBTN = document.getElementById('pniotBTN');
+  
+  moodleBTN.addEventListener('click', () => {
+      window.open('https://moodlearn.ariel.ac.il/auth/multioauth/login.php?userType=student');
+  });
+  
+  meidaBTN.addEventListener('click', () => {
+      window.open('https://meyda.ariel.ac.il/SSOPT/?userType=1');
+  });
+  
+  pniotBTN.addEventListener('click', () => {
+      window.open('http://www.ariel.ac.il/projects/tzmm/secure/login2.asp?student=true');
+  });
+  
+});
+
